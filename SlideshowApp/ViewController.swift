a6c60d917ddb22dd968cf4272a3e7ecf96a503bb
kimura.shinnosuke <shiinosuke.kimura79@gmail.com>
2023-01-24 20:31:43 +0900

    Initial Commit

---
+++ SlideshowApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SlideshowApp
+//
+//  Created by 布団 on 2023/01/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
